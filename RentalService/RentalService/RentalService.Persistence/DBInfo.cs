ce3fb2a56da94c3d0680d5729eb57e3c5d759991
ChristopheBill <christophe.billiet@hotmail.com>
2025-05-09 12:29:57 +0200

    Updated info

--- RentalService/RentalService/RentalService.Persistence/DBInfo.cs
+++ RentalService/RentalService/RentalService.Persistence/DBInfo.cs
@@ -2,6 +2,6 @@
 {
     internal static class DBInfo
     {
-        public const string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""C:\Users\Christophe\Documents\HoGent\2025\Programmeren Gevorderd 1\EINDOPDRACHT\RentalService\RentalService\RentalService.Database\RentalServiceDB.mdf"";Integrated Security=True;Connect Timeout=30";
+        public const string ConnectionString = @"C:\ProjectenVS\EindopdrachtPROG\RentalService\RentalService\RentalService.Database\RentalServiceDB.mdf";
     }
 }
