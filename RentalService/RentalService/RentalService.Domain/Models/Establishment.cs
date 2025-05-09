822198558b94724cf27cbd168e66fa5c30701172
ChristopheBill <christophe.billiet@hotmail.com>
2025-05-09 13:41:12 +0200

    reservation

--- RentalService/RentalService/RentalService.Domain/Models/Establishment.cs
+++ RentalService/RentalService/RentalService.Domain/Models/Establishment.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RentalService.Domain.Models
 {
-    internal class Establishment
+    public class Establishment
     {
         public Establishment(string airport, string streetName, int postalCode, int city, string country)
         {
---
+++ RentalService/RentalService/RentalService.Domain/Models/Reservation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalService.Domain.Models
+{
+    public class Reservation
+    {
+        public Reservation(int id, Customer customer, Car car, Establishment establishment, DateTime startDatum, DateTime eindDatum)
+        {
+            Id = id;
+            Customer = customer;
+            Car = car;
+            Establishment = establishment;
+            StartDatum = startDatum;
+            EindDatum = eindDatum;
+        }
+
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public Car Car { get; set; }
+        public Establishment Establishment { get; set; }
+        public DateTime StartDatum { get; set; }
+        public DateTime EindDatum { get; set; }
+
+        public void Valideer()
+        {
+            if (StartDatum < DateTime.Now)
+                throw new ArgumentException("Startdatum moet in de toekomst liggen.");
+            if (EindDatum <= StartDatum)
+                throw new ArgumentException("Einddatum moet na startdatum liggen.");
+            if ((EindDatum - StartDatum).TotalDays < 1)
+                throw new ArgumentException("Minimum huurperiode is 1 dag.");
+        }
+    }
+}
