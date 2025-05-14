5f192e0c568c5b7bbd7bad0ec10cff1cf420d283
ChristopheBill <christophe.billiet@hotmail.com>
2025-05-14 15:10:05 +0200

    fixed reservationmapper

--- RentalService/RentalService/RentalService.Domain/Repositories/IReservationRepository.cs
+++ RentalService/RentalService/RentalService.Domain/Repositories/IReservationRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RentalService.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace RentalService.Domain.Repositories
 {
     public interface IReservationRepository
     {
+        public List<Reservation> GetReservations(Customer customer, Car car, Establishment establishment);
 
     }
 }
--- RentalService/RentalService/RentalService.Persistence/Mappers/ReservationMapper.cs
+++ RentalService/RentalService/RentalService.Persistence/Mappers/ReservationMapper.cs
@@ -11,32 +11,32 @@ namespace RentalService.Persistence.Mappers
 {
     public class ReservationMapper : IReservationRepository
     {
-        //public List<Establishment> GetReservations(Customer customer, Car car, Establishment establishment)
-        //{
-        //    using SqlConnection connection = new(DBInfo.ConnectionString);
-        //    connection.Open();
-        //    using SqlCommand command = new("SELECT * FROM Reservations", connection);
-        //    command.Parameters.AddWithValue("@CarId", car.Id);
-        //    command.Parameters.AddWithValue("@CustomerId", customer.Id);
-        //    command.Parameters.AddWithValue("@BranchId", establishment.Id);
+        public List<Reservation> GetReservations(Customer customer, Car car, Establishment establishment)
+        {
+            using SqlConnection connection = new(DBInfo.ConnectionString);
+            connection.Open();
+            using SqlCommand command = new("SELECT * FROM Reservations", connection);
+            command.Parameters.AddWithValue("@CarId", car.Id);
+            command.Parameters.AddWithValue("@CustomerId", customer.Id);
+            command.Parameters.AddWithValue("@BranchId", establishment.Id);
 
-        //    using SqlDataReader reader = command.ExecuteReader();
+            using SqlDataReader reader = command.ExecuteReader();
 
-        //    List<Reservation> reservations = [];
+            List<Reservation> reservations = [];
 
-        //    if (reader.HasRows)
-        //    {
-        //        while (reader.Read())
-        //        {
-        //            int id = (int)reader["Id"];
-        //            DateTime startDate = (DateTime)reader["StartDate"];
-        //            DateTime endDate = (DateTime)reader["EndDate"];
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    int id = (int)reader["Id"];
+                    DateTime startDate = (DateTime)reader["StartDate"];
+                    DateTime endDate = (DateTime)reader["EndDate"];
 
-        //            Reservation reservation = new(id, startDate, endDate, customer, car, establishment);
-        //            reservations.Add(reservation);
-        //        }
-        //    }
-        //    return reservations;
-        //}
+                    Reservation reservation = new(id, startDate, endDate, customer, car, establishment);
+                    reservations.Add(reservation);
+                }
+            }
+            return reservations;
+        }
     }
 }
